b28e5886e23cb0b6ecf2dc9b40cf26b63248dc30
Tikochato <daor01@gmail.com>
2019-01-24 02:39:12 -0600

    -Agregando menu responsivo

--- src/features/home/MenuView.js
+++ src/features/home/MenuView.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+
 import PersonasIcon from '@material-ui/icons/Face';
 import ReunionesIcon from '@material-ui/icons/DateRange';
 import ReporteIcon from '@material-ui/icons/InsertChart';
@@ -23,7 +29,25 @@ const styles = theme => ({
     display: 'flex',
   },
   appBar: {
-    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 20,
+  },
+  hide: {
+    display: 'none',
   },
   drawer: {
     width: drawerWidth,
@@ -32,11 +56,29 @@ const styles = theme => ({
   drawerPaper: {
     width: drawerWidth,
   },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
   content: {
     flexGrow: 1,
     padding: theme.spacing.unit * 3,
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
   },
-  toolbar: theme.mixins.toolbar,
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
 });
 
 export class MenuView extends Component {
@@ -44,15 +86,41 @@ export class MenuView extends Component {
     classes: PropTypes.object.isRequired,
   };
 
+  state = {
+    open: false,
+  };
+
+  handleDrawerOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleDrawerClose = () => {
+    this.setState({ open: false });
+  };
+
   render() {
-    const { classes } = this.props;
+    const { classes, theme } = this.props;
+    const { open } = this.state;
 
     return (
       <div className="home-menu-view">
         <div className={classes.root}>
           <CssBaseline />
-          <AppBar position="fixed" className={classes.appBar}>
-            <Toolbar>
+          <AppBar
+            position="fixed"
+            className={classNames(classes.appBar, {
+              [classes.appBarShift]: open,
+            })}
+          >
+            <Toolbar disableGutters={!open}>
+              <IconButton
+                color="inherit"
+                aria-label="Open drawer"
+                onClick={this.handleDrawerOpen}
+                className={classNames(classes.menuButton, open && classes.hide)}
+              >
+                <MenuIcon />
+              </IconButton>
               <Typography variant="h6" color="inherit" noWrap>
                 ScheduleEasy
               </Typography>
@@ -60,12 +128,19 @@ export class MenuView extends Component {
           </AppBar>
           <Drawer
             className={classes.drawer}
-            variant="permanent"
+            variant="persistent"
+            anchor="left"
+            open={open}
             classes={{
               paper: classes.drawerPaper,
             }}
           >
-            <div className={classes.toolbar} />
+            <div className={classes.drawerHeader}>
+              <IconButton onClick={this.handleDrawerClose}>
+                {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+              </IconButton>
+            </div>
+            <Divider />
             <List>
               <NavLink to="/personas" style={{ textDecoration: 'none' }}>
                 <ListItem button>
@@ -96,8 +171,13 @@ export class MenuView extends Component {
               </NavLink>
             </List>
           </Drawer>
-          <main className={classes.content}>
-            <div className={classes.toolbar} />
+
+          <main
+            className={classNames(classes.content, {
+              [classes.contentShift]: open,
+            })}
+          >
+            <div className={classes.drawerHeader} />
             {this.props.body}
           </main>
         </div>
@@ -106,4 +186,4 @@ export class MenuView extends Component {
   }
 }
 
-export default withStyles(styles)(MenuView);
+export default withStyles(styles, { withTheme: true })(MenuView);
